a6c735bf52392e21ef61545940df16fe6e1b7ecf
Sarah Augustin <Sarah.Augustin@viadee.de>
2024-06-07 13:47:13 +0200

    fix bug in input validation mutator

--- packages/instrumenter/src/mutators/new-input-validation-mutator.ts
+++ packages/instrumenter/src/mutators/new-input-validation-mutator.ts
@@ -24,31 +24,44 @@ export const newInputValidationMutator: NodeMutator = {
 
   *mutate(path) {
     if (isObjectRelatedToForms(path)) {
-      const replacement = types.cloneNode(path.node);
-      const array = types.isArrayExpression(replacement) ? replacement.elements : replacement.arguments;
+      const array = types.isArrayExpression(path.node) ? path.node.elements : path.node.arguments;
 
       // delete third argument if it exists and if it is not null, undefined or an empty array
       if (array.length === 3 && !isNullOrUndefinedOrEmptyArray(array[2])) {
-        array.pop();
+        const replacement = types.cloneNode(path.node);
+        types.isArrayExpression(replacement) ? replacement.elements.pop() : replacement.arguments.pop();
         yield replacement;
       }
 
-      // handle second argument if it exists and if it is not null, undefined, an empty array or an object
-      if (array.length >= 2 && !isNullOrUndefinedOrEmptyArray(array[1]) && !types.isObjectExpression(array[1])) {
-        array.length === 3 ? (array[1] = types.arrayExpression()) : array.pop();
+      // delete second argument if exacly two arguments exist, if it is not null, undefined, an empty array, or an object
+      if (array.length === 2 && !isNullOrUndefinedOrEmptyArray(array[1]) && !types.isObjectExpression(array[1])) {
+        const replacement = types.cloneNode(path.node);
+        types.isArrayExpression(replacement) ? replacement.elements.pop() : replacement.arguments.pop();
         yield replacement;
       }
     }
 
-    // if a second argument exists and it is an Object with at least one property being a validator or an asyncValidator, delete those validators
+    // replace the second argument with an empty array if exacly three arguments exist and if the second one is not null, undefined, an empty array, or an object
     if (
       isIthArgumentOfObjectRelatedToForms(path, 1) &&
-      path.isObjectExpression() &&
-      path.node.properties.some((property) => isAValidatorOrAsyncValidator(property))
+      !path.isObjectExpression() &&
+      !isNullOrUndefinedOrEmptyArray(path.node) &&
+      path.parentPath &&
+      isObjectRelatedToForms(path.parentPath)
     ) {
-      const replacement = types.cloneNode(path.node);
-      replacement.properties = path.node.properties.filter((property) => !isAValidatorOrAsyncValidator(property));
-      yield replacement;
+      const array = types.isArrayExpression(path.parentPath.node) ? path.parentPath.node.elements : path.parentPath.node.arguments;
+      if (array.length === 3) yield types.arrayExpression();
+    }
+
+    // if a second argument exists and it is an Object, iterate over its properties and delete them if they are a validator or asyncValidator
+    if (isIthArgumentOfObjectRelatedToForms(path, 1) && path.isObjectExpression()) {
+      for (const [index, property] of path.node.properties.entries()) {
+        if (isAValidatorOrAsyncValidator(property)) {
+          const replacement = types.cloneNode(path.node);
+          replacement.properties.splice(index, 1);
+          yield replacement;
+        }
+      }
     }
   },
 };
